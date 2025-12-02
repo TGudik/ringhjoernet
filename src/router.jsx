f47bb2fecce3e8ad21abbd442c448f0dfe017d99
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-12-02 23:46:07 +0100

    kobler router til main.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 
 /* site import */
-import 
+import Home from "./pages/Home";
+import Needs from "./pages/Needs";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+    {
+        path: "/needs",
+        element: <Needs/>,
+    },
+    {
+        path: "/products",
+        element: <Products/>,
+    },
+    {
+        path: "/about",
+        element: <About/>,
+    },
+    {
+        path: "/cart",
+        element: <Cart/>,
+    },
+])
+
+
+export default router
