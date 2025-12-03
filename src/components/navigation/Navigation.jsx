1798c4bd141918e2aa437932da487e127061fc37
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-12-03 21:39:20 +0100

    tilføjer footer + retter navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -5,16 +5,19 @@ import styles from "./navigation.module.css"
 export default function Navigation() {
 
     return (
-        <header className={styles.header}>
-            <Link to="/">
-            <img src={logo} alt="Ringhjørnet" />
-            </Link>
-            <nav className={styles.nav}>
-                <Link to="/needs">Alt en bokser skal bruge</Link>
-                <Link to="/products">Udstyr</Link>
-                <Link to="/about">Kontakt / Om</Link>
-                <Link to="/cart">Kurv</Link>
-            </nav>
+        <header>
+            <div className={styles.header}>
+                
+                <Link to="/">
+                <img src={logo} alt="Ringhjørnet" />
+                </Link>
+                <nav className={styles.nav}>
+                    <Link to="/needs">Alt en bokser skal bruge</Link>
+                    <Link to="/products">Udstyr</Link>
+                    <Link to="/about">Kontakt / Om</Link>
+                    <Link to="/cart">Kurv</Link>
+                </nav>
+            </div>
         </header>
     )
 
